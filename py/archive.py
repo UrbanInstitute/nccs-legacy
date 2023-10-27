dbe23de68d01df2627beb6eae1e11f35c64ee5e1
Thiyaghessan <thiyaghessan@gmail.com>
2023-10-27 10:13:01 -0400

    Call postprocessing() in main script

--- py/archive.py
+++ py/archive.py
@@ -5,6 +5,7 @@ from archive_setup import folder_setup
 from archive_dd_scraper import archive_dd_scraper
 from archive_dd_dl import pyweb_archive
 from archive_dd_fileorg import file_reorg
+from archive_dd_fileorg import postprocessing
 
 logger = logging
 logger.basicConfig(filename="pywebcopy.log",
@@ -23,9 +24,11 @@ if __name__ == "__main__":
     archive_dd_scraper(series = "home", folder = "data")
     pyweb_archive(series = "home", folder = "data")
     file_reorg(series = "home", archive_folder = "data")
+    postprocessing(series = "home", archive_folder = "data")
 
     for dir in SUBDIRS: 
         archive_dd_scraper(series = dir, folder = "dictionary")
         pyweb_archive(series = dir, folder = "dictionary")
-        file_reorg(series = "home", archive_folder = "dictionary")
+        file_reorg(series = dir, archive_folder = "dictionary")
+        postprocessing(series = dir, archive_folder = "dictionary")
     
